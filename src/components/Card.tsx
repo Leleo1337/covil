ff6dcf8afb30d2a36bd782c67f37a796959389c6
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-04-29 18:18:40 -0300

    updated card responsivity

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,9 +3,9 @@ import { cardProps } from "../types/cardProps.ts";
 export default function Card({ icon, title, text }: cardProps) {
    return (
       <>
-         <div className="flex flex-col items-center justify-center w-full h-32 md:h-40 bg-amber-900/10 p-4 rounded-md">
+         <div className="flex flex-col items-center justify-center w-full lg:w-[300px] h-35 bg-amber-900/10 p-2 rounded-md">
             {icon}
-            <h1 className="font-semibold pt-2 text-md text-amber-800">{title}</h1>
+            <h1 className="font-bold pt-2 text-md text-amber-800">{title}</h1>
             <p className="text-sm text-gray-600 text-center">{text}</p>
          </div>
       </>
