28ab414ecea783915bc82adc97253fd5bc546b2e
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-04-30 17:07:59 -0300

    added contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 // hooks
 import { useEffect, useRef, useState } from "react";
-import { ArrowDown, Beer, Handshake, Instagram, Music } from "lucide-react";
 
 // components
 import Menu from "./components/Menu";
 import Card from "./components/Card";
 import GaleryPhoto from "./components/GaleryPhoto";
+import { ArrowDown, Beer, ClockIcon, Handshake, Instagram, MailIcon, MapIcon, Music, PhoneIcon } from "lucide-react";
 
 // utils
 import { galeryPhotos, historyPhotos } from "./utils/imgPathsObj";
@@ -20,15 +20,17 @@ function App() {
    const homeSectionRef = useRef<HTMLElement | null>(null);
    const historySectionRef = useRef<HTMLElement | null>(null);
    const galerySectionRef = useRef<HTMLElement | null>(null);
+   const contactSectionRef = useRef<HTMLElement | null>(null);
 
    useEffect(() => {
       const handleScroll = () => {
          setSticky(window.scrollY > 50);
       };
       window.addEventListener("scroll", handleScroll);
-
+      window.addEventListener("load", handleScroll);
       return () => {
          window.removeEventListener("scroll", handleScroll);
+         window.removeEventListener("load", handleScroll);
       };
    });
 
@@ -42,7 +44,7 @@ function App() {
             <div className="container mx-auto flex flex-row items-center justify-around pt-4 md:justify-between px-4 h-full">
                <h1 className="text-2xl font-bold text-white text-center ">Covil dos Guri</h1>
                <nav className="hidden sm:block">
-                  <ul className="flex items-center gap-3 md:gap-8 text-white text-md">
+                  <ul className="flex items-center gap-3 md:gap-8 text-white text-base">
                      <li
                         onClick={() => handleScrollToRef(homeSectionRef)}
                         className="cursor-pointer hover:text-amber-300 transition ease-in duration-75">
@@ -58,7 +60,11 @@ function App() {
                         className="cursor-pointer hover:text-amber-300 transition ease-in duration-75">
                         Galeria
                      </li>
-                     <li className="cursor-pointer hover:text-amber-300 transition ease-in duration-75">Contato</li>
+                     <li
+                        onClick={() => handleScrollToRef(contactSectionRef)}
+                        className="cursor-pointer hover:text-amber-300 transition ease-in duration-75">
+                        Contato
+                     </li>
                      <li>
                         <button className="bg-amber-600 py-2 px-5 rounded-md cursor-pointer hover:bg-amber-700 transition ease-in duration-75">
                            Reservas
@@ -94,12 +100,11 @@ function App() {
                <div className="absolute top-0 -z-1 w-full h-screen bg-cover bg-center bg-[url(./assets/background.png)]"></div>
                <ArrowDown
                   onClick={() => handleScrollToRef(historySectionRef)}
-                  color="white"
                   size={30}
-                  className="absolute bottom-10 animate-bounce cursor-pointer"
+                  className="absolute bottom-10 animate-bounce cursor-pointer text-white hover:text-amber-300"
                />
             </section>
-            <section ref={historySectionRef} className="bg-amber-900/5 p-4 pt-12 pb-28">
+            <section ref={historySectionRef} className="bg-amber-800/5 p-4 pt-12 pb-28">
                <div className="mx-auto md:max-w-[1500px]">
                   <div className="flex flex-col gap-3 justify-center items-center pt-12">
                      <h1 className="text-3xl font-bold text-amber-900 text-center md:text-4xl">Nossa historia</h1>
@@ -130,12 +135,12 @@ function App() {
                      </div>
                      <div className="lg:w-180 py-12">
                         <h1 className="font-bold text-2xl text-amber-800 pb-4">Um Lugar de Tradição e Amizade</h1>
-                        <p className="text-gray-600 pb-4 md:text-center lg:text-start">
+                        <p className="text-gray-600 pb-4 text-sm md:text-base">
                            Historia - Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi consectetur
                            expedita iure quod molestias, perferendis dolores commodi dolorem minima consequatur velit
                            repellendus debitis repudiandae provident veritatis ipsa! Velit, obcaecati molestiae!
                         </p>
-                        <p className="text-gray-600 md:pl-8 lg:pl-0">
+                        <p className="text-gray-600 text-sm md:text-base">
                            proposta - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, quod aut facilis
                            laborum perspiciatis sed quam voluptate distinctio atque iste pariatur provident, nisi
                            inventore? Accusantium tenetur odio molestias veritatis tempore.
@@ -161,14 +166,14 @@ function App() {
                   </div>
                </div>
             </section>
-            <section ref={galerySectionRef} className="bg-amber-50">
+            <section ref={galerySectionRef} className="bg-amber-900/10">
                <div className="container mx-auto p-4 pt-20">
                   <div className="flex flex-col justify-center items-center gap-4">
                      <h1 className="text-amber-800 text-4xl font-bold">Nossa Galeria</h1>
                      <p className="text-gray-600">Momentos da Gurizada no covil</p>
                      <div className="w-20 bg-amber-500 h-1"></div>
                   </div>
-                  <div className="grid grid-cols-2 md:grid-cols-3 grid-rows-2 h-full gap-4 pt-16 pb-8">
+                  <div className="grid grid-cols-2 md:grid-cols-3 grid-rows-2 h-full gap-4 pt-16 pb-8 max-h-[1000px]">
                      <GaleryPhoto img={galeryPhotos.galeryFirst} text="Essa noite foi Louca..." />
                      <GaleryPhoto img={galeryPhotos.galerySecond} text="Maromba & mansão maromba..." />
                      <GaleryPhoto img={galeryPhotos.galeryThird} text="Um verdadeiro carro vermelho" />
@@ -194,7 +199,125 @@ function App() {
                   </div>
                </div>
             </section>
-            <section className="h-screen bg-amber-950/10"></section>
+            <section ref={contactSectionRef} className="bg-amber-800/5">
+               <div className="container mx-auto pt-16 p-4">
+                  <div className="flex flex-col justify-center items-center gap-4 pb-16">
+                     <h1 className="text-2xl md:text-4xl text-amber-800 font-bold">Localização & Contato</h1>
+                     <p className="text-gray-600 text-sm text-center md:text-base">
+                        Encontre-nos facilmente e entre em contato para mas informações ou reservas.
+                     </p>
+                     <div className="w-20 h-1 bg-amber-500"></div>
+                  </div>
+                  <div className="grid md:grid-cols-2 gap-12">
+                     <div className="shadow-md">
+                        <iframe
+                           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3496.358084737092!2d-51.60939783339293!3d-28.7984010613808!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x951dc90010e4898b%3A0x20bb891982dd5824!2sCovil%20dos%20Guri!5e0!3m2!1spt-BR!2sbr!4v1746036561230!5m2!1spt-BR!2sbr"
+                           className="h-full w-full rounded-md min-h-60"
+                           loading="lazy"></iframe>
+                     </div>
+                     <div className="flex flex-col gap-8">
+                        <div className="bg-white p-6 rounded-md shadow-md">
+                           <h3 className="text-amber-800 text-xl font-bold">Informações de contato</h3>
+                           <div className="pt-6">
+                              <div className="pt-0">
+                                 <div className="flex gap-4 pb-2">
+                                    <MapIcon className="text-amber-500" />
+                                    <h1 className="font-semibold border-b border-gray-300">Endereço</h1>
+                                 </div>
+                                 <div className="pl-10 text-sm md:text-base text-gray-600">
+                                    <p>Av. Placidina de Araújo - Centro</p>
+                                    Nova Prata - RS, 95320-000
+                                 </div>
+                              </div>
+                              <div className="pt-4">
+                                 <div className="flex gap-4 pb-2">
+                                    <ClockIcon className="text-amber-500" />
+                                    <h1 className="font-semibold border-b border-gray-300">Horário de Funcionamento</h1>
+                                 </div>
+                                 <div className="pl-10 text-sm md:text-base text-gray-600">
+                                    <p>Terça a Quinta: 18h às 00h </p>
+                                    <p>Sexta e Sábado: 18h às 02h </p>
+                                    <p> Domingo: 18h às 00h</p>
+                                    <p>Segunda: Fechado</p>
+                                 </div>
+                              </div>
+                              <div className="pt-4">
+                                 <div className="flex gap-4 pb-2">
+                                    <PhoneIcon className="text-amber-500" />
+                                    <h1 className="font-semibold border-b border-gray-300">Telefone</h1>
+                                 </div>
+                                 <div className="pl-10 text-sm md:text-base text-gray-600">
+                                    <p>(54) 9999-9999</p>
+                                 </div>
+                              </div>
+                              <div className="pt-4">
+                                 <div className="flex gap-4 pb-2">
+                                    <MailIcon className="text-amber-500" />
+                                    <h1 className="font-semibold border-b border-gray-300">E-mail</h1>
+                                 </div>
+                                 <div className="pl-10 text-sm md:text-base text-gray-600">
+                                    <p>contato@covildosguri.com.br</p>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                        <div className="bg-amber-900/8 rounded-md shadow-md">
+                           <div className="p-6">
+                              <h3 className="text-amber-800 text-2xl font-bold pb-4">Faça sua Reserva</h3>
+                              <p className="text-gray-600 text-base">
+                                 Garanta seu lugar para eventos especiais ou um happy hour entre amigos.
+                              </p>
+                           </div>
+                           <div className="px-6 pb-6">
+                              <div className="flex flex-col gap-4">
+                                 <div className="grid md:grid-cols-2 gap-4">
+                                    <input
+                                       type="text"
+                                       placeholder="Nome"
+                                       className="bg-white px-4 py-2 rounded-md border border-gray-300 focus:outline-0 focus:ring-2 focus:ring-amber-400"
+                                    />
+                                    <input
+                                       type="email"
+                                       placeholder="E-mail"
+                                       className="bg-white px-4 py-2 rounded-md border border-gray-300 focus:outline-0 focus:ring-2 focus:ring-amber-400"
+                                    />
+                                 </div>
+                                 <div className="grid md:grid-cols-2 gap-4">
+                                    <input
+                                       type="tel"
+                                       placeholder="telefone"
+                                       className="bg-white px-4 py-2 rounded-md border border-gray-300 focus:outline-0 focus:ring-2 focus:ring-amber-400"
+                                    />
+                                    <input
+                                       type="number"
+                                       placeholder="Numero de pessoas"
+                                       className="bg-white px-4 py-2 rounded-md border border-gray-300 focus:outline-0 focus:ring-2 focus:ring-amber-400"
+                                    />
+                                 </div>
+                                 <div className="grid md:grid-cols-2 gap-4">
+                                    <input
+                                       type="date"
+                                       className="bg-white px-4 py-2 rounded-md border border-gray-300 focus:outline-0 focus:ring-2 focus:ring-amber-400"
+                                    />
+                                    <input
+                                       type="button"
+                                       value={"Selecione o horario"}
+                                       className="bg-white px-4 py-2 rounded-md border border-gray-300 focus:outline-0 focus:ring-2 focus:ring-amber-400"
+                                    />
+                                 </div>
+                                 <textarea
+                                    placeholder="Observações ou pedidos especiais"
+                                    className="bg-white h-26 px-4 py-2 rounded-md border border-gray-300 focus:outline-0 focus:ring-2 focus:ring-amber-400"></textarea>
+                              </div>
+                              <button className="w-full px-4 py-3 mt-4 bg-amber-600 text-white rounded-md">
+                                 Solicitar reserva
+                              </button>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </section>
          </main>
          <footer></footer>
       </>
