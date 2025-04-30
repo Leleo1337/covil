156651c256c1256046f999f6163ddfe216fc6894
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-04-30 17:07:48 -0300

    fixed image height on larger devices

--- src/components/GaleryPhoto.tsx
+++ src/components/GaleryPhoto.tsx
@@ -3,9 +3,9 @@ import { galeryPhotoProps } from '../types/galeryPhotoProps'
 export default function GaleryPhoto({ img, text }: galeryPhotoProps) {
    return (
       <>
-         <div className="group relative flex items-center justify-center overflow-hidden cursor-pointer">
+         <div className="group relative flex items-center justify-center overflow-hidden cursor-pointer rounded-md">
             <img
-               className="rounded-lg h-full w-full hover:brightness-40 hover:scale-105 transition-all ease duration-300 "
+               className="rounded-lg h-full w-full object-cover hover:brightness-40 hover:scale-110 transition-all ease duration-300 "
                src={img}
                alt="First"
             />
