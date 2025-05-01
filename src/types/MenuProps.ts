3a4844cf94e8763084e16d90eb034a0317ad17c5
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-05-01 16:29:05 -0300

    added refs

--- src/types/MenuProps.ts
+++ src/types/MenuProps.ts
@@ -1,4 +1,11 @@
 export type menuProps = {
     isOpen: boolean
+    refs: {
+        homeSectionRef: React.RefObject<HTMLElement | null>;
+        historySectionRef: React.RefObject<HTMLElement | null>
+        galerySectionRef: React.RefObject<HTMLElement | null>;
+        contactSectionRef: React.RefObject<HTMLElement | null>;
+        reserveDivRef: React.RefObject<HTMLDivElement | null>;
+      };
     onClick: () => void
 }
