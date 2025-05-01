f61a769ce71fce4cfd7c16e0c9b4c71209eeb9d8
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-05-01 16:03:26 -0300

    renamed + restructured

---
+++ src/data/imgDatas.ts
@@ -0,0 +1,89 @@
+import historyFirst from "../assets/historyPhotos/first.png";
+import historySecond from "../assets/historyPhotos/second.png";
+import historyThird from "../assets/historyPhotos/third.png";
+import historyFourth from "../assets/historyPhotos/fourth.png";
+
+import galeryFirst from "../assets/galleryPhotos/image1.png";
+import galerySecond from "../assets/galleryPhotos/image2.png";
+import galeryThird from "../assets/galleryPhotos/image3.png";
+import galeryFourth from "../assets/galleryPhotos/image4.png";
+import galeryFifth from "../assets/galleryPhotos/image5.png";
+import galerySixth from "../assets/galleryPhotos/image6.png";
+
+export const galeryPhotos = [
+   {
+      id: 1,
+      img: galeryFirst,
+      text: "Essa noite foi Louca...",
+      fullscreen: false,
+   },
+   {
+      id: 2,
+      img: galerySecond,
+      text: "Maromba & mansão maromba...",
+      fullscreen: false,
+   },
+   {
+      id: 3,
+      img: galeryThird,
+      text: "Um verdadeiro carro vermelho",
+      fullscreen: false,
+   },
+   {
+      id: 4,
+      img: galeryFourth,
+      text: "A mais bela culinaria da casa",
+      fullscreen: false,
+   },
+   {
+      id: 5,
+      img: galeryFifth,
+      text: "Reunião de negocios...",
+      fullscreen: false,
+   },
+   {
+      id: 6,
+      img: galerySixth,
+      text: "Nossos chef's profissionais e altamente qualificados",
+      fullscreen: false,
+   },
+   {
+      id: 7,
+      img: galerySixth,
+      text: "Nossos chef's profissionais e altamente qualificados",
+      fullscreen: false,
+   },   {
+      id: 8,
+      img: galerySixth,
+      text: "Nossos chef's profissionais e altamente qualificados",
+      fullscreen: false,
+   },   {
+      id: 9,
+      img: galerySixth,
+      text: "Nossos chef's profissionais e altamente qualificados",
+      fullscreen: false,
+   },
+      {
+      id: 10,
+      img: galerySixth,
+      text: "Nossos chef's profissionais e altamente qualificados",
+      fullscreen: false,
+   },   {
+      id: 11,
+      img: galerySixth,
+      text: "Nossos chef's profissionais e altamente qualificados",
+      fullscreen: false,
+   },{
+      id: 12,
+      img: galerySixth,
+      text: "Nossos chef's profissionais e altamente qualificados",
+      fullscreen: false,
+   }
+];
+
+export const historyPhotos = {
+   historyFirst,
+   historySecond,
+   historyThird,
+   historyFourth,
+};
